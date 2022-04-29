aa36f09fc5ae0a5df267a0269e81396c6e45dc4f
heyotunes <97762771+heyotunes@users.noreply.github.com>
2022-04-29 01:18:24 -0400

    f

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
+    host : '127.0.0.1',
+    user : 'addyogunmonasotayo',
+    password : '',
+    database : 'face'
   }
 });
 
@@ -39,6 +41,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3001, ()=> {
+app.listen(3001, ()=> {
   console.log('app is running on port 3001');
 })
